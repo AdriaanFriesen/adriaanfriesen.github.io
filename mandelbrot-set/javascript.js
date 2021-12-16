d0f4744f2904ce9ce9bc946c3f4053e77798a9c5
AdriaanFriesen <adriaan.friesen@gmail.com>
2021-12-16 13:34:48 -0500

    Got the set rendering, still buggy as hell

--- mandelbrot-set/javascript.js
+++ mandelbrot-set/javascript.js
@@ -4,21 +4,23 @@ var render_offset = [0, 0];
 var dragging = null;
 
 canvas.addEventListener('mousedown', function(evt) {
-    dragging = true;
+    // dragging = true;
     mouse_pos_start = [evt.offsetX, evt.offsetY];
 }, false)
 
-canvas.addEventListener('mousemove', function(evt) {
-    mouse_pos_new = [evt.offsetX, evt.offsetY];
-    if (dragging) {
-        render_offset = [mouse_pos_new[0] - mouse_pos_start[0], mouse_pos_new[1] - mouse_pos_start[1]];
-        mouse_pos_start = [evt.offsetX, evt.offsetY];
-        render_set();
-    }
-}, false);
+// canvas.addEventListener('mousemove', function(evt) {
+//     if (dragging) {
+//         console.log("drag")
+//     }
+// }, false);
 
 canvas.addEventListener('mouseup', function(evt) {
-    dragging = false;
+    // dragging = false;
+    mouse_pos_new = [evt.offsetX, evt.offsetY];
+    render_offset = [mouse_pos_new[0] - mouse_pos_start[0], mouse_pos_new[1] - mouse_pos_start[1]];
+    mouse_pos_start = [evt.offsetX, evt.offsetY];
+    console.log("mouseup")
+    render_set();
 }, false)
 
 
@@ -26,7 +28,8 @@ window.addEventListener('resize', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    render_set(); 
+    render_set();
+    console.log("resize")
 }, 
 false);
 
@@ -42,50 +45,39 @@ function complex_addition(comp1, comp2) {
     return [comp1[0] + comp2[0], comp1[1] + comp2[1]]
 }
 
-function set_check(c, x, y, lowestval) {
+function set_check(c, x, y) {
     let z = [0, 0];
     let max_i = 10;
     let i = max_i;
-    if (Math.abs(c[0]) < lowestval) {
-        lowestval = Math.abs(c[0]);
-    }
-    // if (Math.abs(c[0]) < 0.1 && Math.abs(c[1]) < 0.1) {
-    //     console.log(c);
-    //     console.log(x, y);
-    // }
     while (complex_absolute(z) < 2 && i >= 0) {
-        // console.log(z[0], z[1]);
         z = complex_addition(complex_squaring(z), c)
-        // console.log(z[0], z[1]);
         i--;
     }
     if (complex_absolute(z) < 2) {
         drawPixel(gl, x, y)
-        console.log("pixel")
+        // console.log("pixel")
     }
     if (complex_absolute(z) < 2.1) {
     }
-    // console.log(z[0], z[1]);
 }   
 
 function render_set() {
 
-    console.log("render");
+    // console.log("render");
 
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     var highest_dim = Math.max(canvas.width, canvas.height);
 
-    var lowestval = 10.0;
-    
-    for (var y = 0; y <= canvas.height + 0; y += 1) {
-        for (var x = 0; x <= canvas.width + 0; x += 1) {
-            set_check([ ( ( ( x / highest_dim ) * 1 ) - ( ( canvas.width / highest_dim ) * 2 ) ), ( ( ( y / highest_dim ) * 1 ) - ( ( canvas.height / highest_dim ) * 2 ) ) ], x, y, lowestval);
+    for (var y = render_offset[1]; y <= canvas.height + render_offset[1]; y += 4) {
+        for (var x = render_offset[0]; x <= canvas.width + render_offset[0]; x += 4) {
+            set_check([ ( ( ( x / highest_dim ) * 4 ) - ( ( canvas.width / highest_dim ) * 4 ) ), ( ( ( y / highest_dim ) * 4 ) - ( ( canvas.height / highest_dim ) * 4 ) ) ], x, y);
             // console.log(x, y)
         }
+        // console.log(Number(((y / canvas.height) * 100).toFixed(1)) + "%");
     }
-    console.log(lowestval); 
+    // console.log("done render");
 }
 
 render_set();
